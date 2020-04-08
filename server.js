63bd24663a7116ca72f5beeef882fffa715f5965
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-04-07 21:32:26 -0700

    added compression package for sick gainz, yo

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const compression = require("compression");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
@@ -6,6 +7,7 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(compression());
 
 // require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
